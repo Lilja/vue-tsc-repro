85b4ce6ca67bebb21ff67a363262ea8a73b51924
Erik Lilja <erik.lilja@funnel.io>
2022-06-08 10:09:49 +0200

    Init commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import Table from "./Table.vue";
+
+defineProps<{ msg: string }>();
+
+const count = ref(0);
+</script>
+
+<template>
+  <Table>
+    <template
+      v-for="(_, name) in ($slots as Pick<typeof $slots, 'default'| 'actions'| 'expanded'>)"
+      v-slot:[name]="slotData"
+    >
+      <slot :name="name" v-bind="slotData" />
+    </template>
+  </Table>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
---
+++ src/components/Table.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <slot name="actions" />
+  </div>
+</template>
+
+<script lang="ts" setup></script>
